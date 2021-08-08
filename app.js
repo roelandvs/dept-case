7276c0b93583f6df0cfd26e93ddcd24564ef0ff6
Roeland van Stee <roelandvanstee@gmail.com>
2021-08-08 18:54:41 +0200

    add cases obj

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const { tileContent } = require('./data/tileContent');
+
 app
     .use(express.static('./public'))
     .set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-    res.render('pages/index')
+    res.render('pages/index', {data: tileContent})
 })
 
 app.listen(port, () => {
---
+++ data/tileContent.js
@@ -0,0 +1,64 @@
+const tileContent = [
+    {
+        client: 'Bol.com',
+        title: 'A summer island in the Netherlands',
+        img: 'bolcom.png',
+        url: '#'
+    },
+    {
+        client: 'Kempen',
+        title: 'Not some average banking website',
+        img: 'kempen.png',
+        url: '#'
+    },
+    {
+        client: 'Philips',
+        title: 'Beautiful design meets innovative technology',
+        img: 'philips.png',
+        url: '#'
+    },
+    {
+        client: 'Gemeentemusem',
+        title: 'A 100 years of Mondriaan & De Stijl',
+        img: 'gemeentemuseum.png',
+        url: '#'
+    },
+    {
+        client: 'Be Lighting',
+        title: 'Delivering clarity on a global scale',
+        img: 'lightning.png',
+        url: '#'
+    },
+    {
+        client: 'TUI',
+        title: 'Swipe to find your next holiday destination',
+        img: 'tui.png',
+        url: '#'
+    },
+    {
+        client: 'Chocomel',
+        title: 'A campaign for the limited edition letter packs',
+        img: 'chocomel.png',
+        url: '#'
+    },
+    {
+        client: 'JBL',
+        title: 'Live like a champion with Jerome Booteng',
+        img: 'jbl.png',
+        url: '#'
+    },
+    {
+        client: 'ZALANDO',
+        title: 'Innovative SEO and content strategy for Zalando',
+        img: 'zalando.png',
+        url: '#'
+    },
+    {
+        client: 'Koninklijke Bibliotheek',
+        title: 'The search for the most influential book ever',
+        img: 'koninklijke-bibliotheek.png',
+        url: '#'
+    }
+];
+
+module.exports = { tileContent };
