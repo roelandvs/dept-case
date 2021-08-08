2fbabce73a3e6288012042695157cfbf74ddf335
Roeland van Stee <roelandvanstee@gmail.com>
2021-08-08 10:42:29 +0200

    add express boilerplate

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app
+    .use(express.static('./public'))
+    .set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render('pages/index')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+});
