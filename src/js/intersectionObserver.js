104791f72253d2b53843125adfad90c880b36c32
Roeland van Stee <roelandvanstee@gmail.com>
2021-08-08 22:07:17 +0200

    add intersection observer

---
+++ src/js/intersectionObserver.js
@@ -0,0 +1,18 @@
+const projects = document.querySelectorAll('.case');
+const root = document.documentElement;
+
+root.style.setProperty('--case-opacity', '0');
+
+let observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if(entry.intersectionRatio > 0) {
+            console.log(entry.intersectionRatio)
+            entry.target.style.animation = 'animIn 1s forwards';
+            observer.unobserve(entry.target);
+        };    
+    });
+});
+
+projects.forEach(project => {
+    observer.observe(project);
+});
