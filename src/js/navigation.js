aba1bfcb7a07a052263eed8a3b5b68384252c1ea
Roeland van Stee <roelandvanstee@gmail.com>
2021-08-09 08:34:39 +0200

    add nav change on scroll

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -5,4 +5,13 @@ function openNav() {
     nav.classList.toggle('open');
 };
 
+// window.onscroll is bad for performance
+window.onscroll = () => {
+    if (window.scrollY > 10) {
+        nav.classList.add('scroll');
+    } else {
+        nav.classList.remove('scroll');
+    }
+};
+
 navButton.addEventListener('click', openNav);
